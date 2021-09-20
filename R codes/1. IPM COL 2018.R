2b59c5c30f5642c3e7e47fb184068fc82bd1a55d
dflemus <dflemusp@gmail.com>
2021-09-19 20:44:46 -0500

    Código SAE dimensiones IPM

---
+++ R codes/1. IPM COL 2018.R
@@ -0,0 +1,221 @@
+#################
+### Librerías ###
+#################
+
+library(tidyverse)
+library(dplyr)
+library(readstata13)
+library(sas7bdat)
+
+# COL18 <- read.dta13("Input/col18n1/DATA/col18n1.dta")
+# saveRDS(COL18, "Input/COL18.rds")
+#
+# COL18 <- readRDS("Input/COL18.rds")
+# 
+# # Variables de privacion
+# 
+# COL18_2 = COL18 %>% select(id_hogar, id_pers,persindo, areageo2, factorex, edad, sexo, p5090, p4030s5, p5050,
+#                  p5020, p5030, p4030s1,p4000, p4020, p4010, p5080, p6160, p6170,
+#                  condact, p5000, p5010, p5210s16, p5210s3, anoest, 
+#                  p6920 , p6980s1 , p6980s2 , p7420s1 , p7420s2 , p7460,
+#                  iof2, iof2es, categ5_p)
+# saveRDS(COL18_2, "Input/COL18_baseIPM.rds")
+
+################################################################################
+                    ### Lectura de bases de datos ###
+################################################################################
+
+COL18_2 <- readRDS("Input/COL18_baseIPM.rds")
+
+IPM <- COL18_2 %>% transmute(
+  
+  ###------------------ Identificador de hogar y personas -------------------### 
+  
+  id_hogar, id_pers,
+  
+  ###--------------------------- Área geográfica ----------------------------### 
+  
+  area = areageo2,
+  
+  ###--
+  factorex,
+  edad, sexo,
+  
+  # generadas desde basevariable
+  nbi_ten = case_when(p5090 %in% c(5,6) ~ 1,
+                      p5090 == 9 ~ NA_real_,
+                      TRUE ~ 0),
+  nbi_agua = case_when(p4030s5 == 2 & area == 2 ~ 1,
+                       p4030s5 == 1 ~ 1,
+                       p5050 %in% c(1, 2, 3) ~ 0,
+                       p5050 %in% c(4, 5, 6) ~ 0,
+                       p5050 %in% c(7, 8, 9) & area == 1 ~ 1,
+                       p5050 %in% c(7, 8, 9) & area == 2 ~ 0,
+                       p5050 == 10 ~ 0,
+                       is.na(p5050) ~ NA_real_,
+                       p4030s5 == 2 & area == 1 ~ 1),
+  nbi_saneamiento = case_when(p5020 %in% c(1,2) ~ 0,
+                              p5020 %in% c(2,3,4,5) & area == 1 ~ 1,
+                              p5020 %in% c(5) & area == 2 ~ 1,
+                              p5020 == 6 ~ 1,
+                              p5030 == 2 ~ 1),
+  nbi_elect =  case_when(p4030s1 %in% c(2) ~ 0,
+                         p4030s1 == 1 ~ 1,
+                         p4030s1 == 0 ~ NA_real_),
+  nbi_piso =   case_when(p4020 %in% c(2:7) ~ 0,
+                         p4020 == 1 ~ 1,
+                         TRUE ~ NA_real_),
+  nbi_pared =  case_when(p4010 %in% c(6:9) ~ 1,
+                         TRUE ~ 0),
+  nbi_combus =  case_when(p5080 %in% c(2,5,6,7) ~ 1,
+                        TRUE ~ 0),
+  lee_ee = case_when(p6160 == 1 ~ 1,
+                     p6160 == 2 ~ 0,
+                     is.na(p6160) ~ NA_real_),
+  asiste_ee = case_when(p6170 == 1 ~ 1,
+                       p6170 == 2 ~ 0,
+                       is.na(p6170) ~ NA_real_),
+  condactr = case_when(condact == -1 ~ NA_real_,
+                       condact == 1 ~ 1,
+                       condact == 2 ~ 2,
+                       condact == 3 ~ 2,
+                       condact == 4 ~ 3,
+                       condact == 5 ~ 4,
+                       condact == 6 ~ 7,
+                       condact == 7 ~ 6),
+  # Generadas desde do - file
+  nbi_matviv = case_when((nbi_piso == 1 | nbi_pared ==1) ~ 1,
+                         (nbi_piso == 0 & nbi_pared ==0) ~ 0,
+                         p4000 %in% c(5,6) ~ 1,
+                         p4010 == 5 ~ 1,
+                         p4020 == 3 ~ 1),
+   nbi_energia = case_when(nbi_elect == 1 | nbi_combus == 1 ~ 1,
+                           nbi_elect == 0 & nbi_combus == 0 ~ 0,
+                           is.na(nbi_elect) & is.na(nbi_combus) ~ NA_real_),
+  nbi_asistencia_ee = ifelse(asiste_ee == 0 & (edad>= 4 & edad <= 18), 1, 0),
+  ncuartos_ee = ifelse(p5000 == -1, NA_real_, p5000),
+  ndormitorios_ee = ifelse(p5010 == -1, NA_real_, p5010),
+  nbi_hacina21 = case_when(ndormitorios_ee == 0 ~ 1,
+                           (persindo/ndormitorios_ee) > 2 ~ 1,
+                           TRUE ~ 0),
+  nbi_compuhog_ee = case_when(p5210s16 == 2 ~ 1,
+                              p5210s16 == 1 ~ NA_real_),
+  nbi_interhog_ee = ifelse(p5210s3 == 2, 1, 0),
+  niveduc_ee = case_when(anoest < 5 ~ 1,
+                         anoest == 5 ~ 2,
+                         anoest > 5 & anoest < 9 ~ 3,
+                         anoest >= 9 & anoest < 11 ~ 4,
+                         anoest == 11 ~ 5,
+                         anoest > 11 & anoest < 16 ~ 6,
+                         anoest >= 16 ~ 7),
+  rezagado = case_when(edad < 6 | edad > 16 ~ NA_real_,
+                       asiste_ee == 1 & (edad - 6 - anoest) > 2 ~ 1),
+  rezagado = ifelse(is.na(rezagado), 0, 1),
+  nbi_logro_sc = case_when(niveduc_ee >= 5 & edad >=  19 & edad <= 29~ 0,
+                           niveduc_ee < 5 & edad >=  19 & edad <= 29 ~ 1,
+                          TRUE  ~ NA_real_),
+  nbi_logro_bs = case_when(niveduc_ee > 3 & edad >=  30 & edad <= 59~ 0,
+                           niveduc_ee <= 3 & edad >=  30 & edad <= 59 ~ 1,
+                           TRUE  ~ NA_real_),
+  afilia = ifelse(p6920 == 1 | p6980s1 == 1 | p6980s2 == 1 | p7420s1 == 1 | p7420s2 == 1 | p7460 == 1,1,0),
+  cotiza = ifelse(p6920 == 1 | p6980s1 == 1 | p6980s2 == 1 | p7420s1 == 1 | p7420s2 == 1, 1, 0),
+  #cotizadef
+  Tptotal = case_when(is.na(iof2) & is.na(iof2es) ~ 0,
+                      iof2 > 0 | iof2es > 0 ~ 1,
+                      TRUE ~ 0),
+  adultos = ifelse((edad>=18 & edad<60 & sexo==2) | (edad>=18 & edad<65 & sexo==1), 1, 0),
+  a_mayores = ifelse((edad>=60 & sexo==2)| (edad>=65 & sexo==1), 1, 0),
+  nbi_tics_h = ifelse(nbi_compuhog_ee==1 & nbi_interhog_ee==1, 1, 0),
+  nbi_asan = ifelse((nbi_agua==1 | nbi_saneamiento==1), 1, 0),
+  noasis18 = ifelse(nbi_asistencia_ee==0 & niveduc_ee<5 & edad>=18 & edad<=19, 1, 0),
+  nbi_asrez = ifelse((nbi_asistencia_ee==1 | rezagado==1 | noasis18==1 ) & edad<20, 1, 0),
+  nbi_conclued = ifelse((nbi_logro_sc == 1 | nbi_logro_bs == 1 | noasis18==1),1,0),
+  analfab_ee = case_when(lee_ee == 0 ~ 1,
+                         lee_ee == 1 ~ 0,
+                         is.na(lee_ee) ~ NA_real_),
+  amay_analfab = ifelse((analfab_ee == 1 & a_mayores==1), 1, 0),
+  ocup_priv2 =ifelse((condactr == 2 | categ5_p == 5 | (cotiza==0 & condactr ==1) | condactr == 4) , 1, 0),
+  nbi_pen = case_when(a_mayores == 0 ~ NA_real_,
+                      a_mayores == 1 & Tptotal == 0 ~ 1,
+                      TRUE ~ 0),
+  
+  # Gaps de dimension VIVIENDA para los tres grupos
+  
+  g0_A_nbi_matviv = ifelse(adultos == 1,(1/8)*nbi_matviv, 0),
+  
+  g0_A_nbi_hacina21 = ifelse(adultos == 1,(1/8)*nbi_hacina21, 0),
+  
+  g0_AM_nbi_matviv = ifelse(a_mayores == 1,(1/8)*nbi_matviv, 0),
+  
+  g0_AM_nbi_hacina21 = ifelse(a_mayores == 1,(1/8)*nbi_hacina21, 0),
+  
+  # Gaps de dimension servicios
+  
+  g0_A_nbi_tics_h = ifelse(adultos == 1,(1/16)*nbi_tics_h,0),
+  g0_A_nbi_agua = ifelse(adultos == 1,(1/16)*nbi_agua,0),
+  g0_A_nbi_nbi_saneamiento = ifelse(adultos == 1,(1/16)*nbi_saneamiento,0),
+  g0_A_nbi_nbi_energia = ifelse(adultos == 1,(1/16)*nbi_energia,0), 
+  
+  g0_AM_nbi_tics_h = ifelse(a_mayores == 1,(1/16)*nbi_tics_h,0),
+  g0_AM_nbi_agua = ifelse(a_mayores == 1,(1/16)*nbi_agua,0),
+  g0_AM_nbi_nbi_saneamiento = ifelse(a_mayores == 1,(1/16)*nbi_saneamiento,0),
+  g0_AM_nbi_nbi_energia = ifelse(a_mayores == 1,(1/16)*nbi_energia,0), 
+  
+  # Gaps IPM adultos
+  
+  g0_A_nbi_conclued = ifelse(adultos == 1,(1/4)*nbi_conclued ,0),
+  g0_A_ocup_priv2 = ifelse(adultos == 1,(1/4)*ocup_priv2,0),
+  
+  # Gaps Adultos mayores
+  
+  g0_AM_amay_analfab  = ifelse(adultos == 1,(1/4)*amay_analfab ,0),
+  g0_AM_nbi_pen = ifelse(adultos == 1,(1/4)*nbi_pen,0),
+  )
+
+# Cantidad privaciones adulto
+IPM$C_A = rowSums(IPM %>% select(starts_with("g0_A")), na.rm = T)
+IPM$C_A = ifelse(IPM$adultos == 0, NA, IPM$C_A)
+# Cantidad privaciones adulto mayor
+IPM$C_AM = rowSums(IPM %>% select(starts_with("g0_AM")), na.rm = T)
+IPM$C_AM = ifelse(IPM$a_mayores == 0, NA, IPM$C_AM)
+summary(IPM$C_A)
+summary(IPM$C_AM)
+
+mean(IPM$C_A)
+weighted.mean(IPM$C_A, IPM$factorex)
+
+
+mean(IPM$C_AM)
+weighted.mean(IPM$C_AM, IPM$factorex)
+
+## 
+IPM$AM_PMD = NA
+IPM$A_PMD = NA
+
+for(k in 1:100){
+  print(k)
+  #IPM$AM_PMD = ifelse(IPM$C_AM >= k/100, paste0("Adulto mayor PM con k =",k),  IPM$AM_PMD)
+  #IPM$A_PMD = ifelse(IPM$C_A >= k/100, paste0("Adulto PM con k =",k),  IPM$A_PMD)
+  
+  IPM$AM_PMD = ifelse(IPM$C_AM >= k/100, k,  IPM$AM_PMD)
+  IPM$A_PMD = ifelse(IPM$C_A >= k/100, k,  IPM$A_PMD)
+  
+}
+IPM$AM_PMD = ifelse(IPM$adultos == 1, NA, IPM$AM_PMD )
+
+
+IPM$c_AM_PMD = ifelse(is.na(IPM$AM_PMD), 0, IPM$AM_PMD)
+IPM$c_A_PMD = ifelse(is.na(IPM$A_PMD), 0, IPM$A_PMD)
+
+
+IPM %>% filter(adultos == 1) %>% mutate(sexo = ifelse(sexo == 1, "Hombre", "Mujer")) %>%
+  group_by(sexo) %>%
+  summarise(IPM_A = weighted.mean(c_A_PMD, factorex))
+
+
+IPM %>% filter(a_mayores == 1) %>% mutate(sexo = ifelse(sexo == 1, "Hombre", "Mujer")) %>%
+  group_by(sexo) %>%
+  summarise(IPM_AM = weighted.mean(c_AM_PMD, factorex))
+
+saveRDS(IPM, "Input/IPM.rds")
+
---
+++ R codes/2. Plug in_AS_FL.R
@@ -0,0 +1,240 @@
+
+###--- Cleaning R environment ---###
+
+rm(list = ls())
+gc()
+
+#################
+### Libraries ###
+#################
+
+library(dplyr)
+library(survey)
+library(srvyr)
+library(sae)
+library(lme4)
+library(data.table)
+library(haven)
+library(magrittr)
+library(formula.tools)
+library(remotes)
+library(StatisticalModels)
+library(fastDummies)
+library(haven)
+library(magrittr)
+select <- dplyr::select
+
+###------------ Definiendo el directorio de trabajo ------------###
+
+setwd("C:/Users/Usuario/Desktop/Ejemplo/Bases")
+
+###------------ Definiendo el límite de la memoria RAM a emplear ------------###
+
+memory.limit(180000000)
+
+################################################################################
+###----------------------------- Loading datasets ---------------------------###
+################################################################################
+
+###--- GEIH: Completa ---###
+
+GEIH2018 <- readRDS("GEIH2018.rds")
+
+###--- GEIH: con variables estandarizadas ---###
+
+Xencuesta <- readRDS("Xencuesta.rds")
+
+###--- Censo: Completo ---###
+
+CensoPersonas <- readRDS("Censo_Completo.rds")
+
+###--- Censo: con variables estandarizadas ---###
+
+Xcenso <- readRDS("Xcenso.rds")
+
+################################################################################
+              ### GEIH: Creando las dimensiones de interés ###
+################################################################################
+
+###--------------- Aseguramiento Salud y formalidad laboral -----------------###
+
+indicadores <- GEIH2018 %>% transmute(
+  idhogar = paste0(directorio,secuencia_p),
+  Aseguramiento_Salud = case_when(edad > 5 & p6090 == 1 ~ 1,
+                                  edad > 5 & p6090 == 2 ~ 0,
+                                  TRUE ~ NA_real_),
+  Formalidad_laboral = case_when(edad > 17 & condact3 == 1 & p6920 %in% c(1,3) ~ 1,
+                                 edad > 17 & condact3 == 1 & p6920 %in% c(2) ~ 0,
+                                 TRUE ~ NA_real_))
+
+###--------------- Anexando variables a la base estandarizada ---------------###
+
+Xencuesta %<>% left_join(indicadores %>% group_by(idhogar) %>%
+                         summarise(Priv_aseguramiento_salud = ifelse(any(Aseguramiento_Salud %in% 0), 1, 0),
+                                   Priv_Formalidad_laboral = ifelse(any(Formalidad_laboral %in% 0), 1, 0)), by = "idhogar")
+
+################################################################################
+###------------ Ajuste del modelo Plugin: aseguramiento en salud ------------###
+################################################################################
+
+paste(names(Xencuesta), collapse = " + ")
+pluginreg_1 <- glmer(Priv_aseguramiento_salud ~ Area + tipo_viv_casa + 
+                       tipo_viv_depto + tipo_viv_cuarto + tipo_viv_indigena + 
+                       mpared1 + mpared2 + mpared3 + mpared4 + mpared5 + 
+                       mpared6 + mpisos1 + mpisos2 + mpisos3 + mpisos4 + 
+                       mpisos5 + agua_alim1 + agua_alim2 + agua_alim3 + 
+                       agua_alim4 + agua_alim5 + agua_alim6  + jefe_Mujer + 
+                       prop_mujeres + electrica_ee + acueducto  + nhijos_hogar2 + 
+                       nhijos_hogar3 + nhijos_hogar4 + alcantarillado + 
+                       prop_alfabeta + gasnatural_redp + rec_basura + internet + 
+                       servhig1 + servhig2 + servhig3 + servhig4 + servhig5 + 
+                       tamhog2 + tamhog3 + tamhog4 + hacinamiento + prep_alim2 + 
+                       prep_alim3 + prep_alim4 + prep_alim5 + prep_alim6 + 
+                       Edad_jefe1 + Edad_jefe2 + Edad_jefe3 + Edad_jefe4 + 
+                       inasistente + Jefe_sup + ratio_prim + ratio_media + 
+                       ratio_sup + prop_ocupados + prop_inactivos + 
+                       trabajo_infantil + r_solt + r_casad + migrante_cortop +
+                       migrante_medianop + (1|Municipio), family = "binomial", 
+                     data = Xencuesta)
+
+################################################################################
+###-------------- Ajuste del modelo Plugin: Formalidad laboral --------------###
+################################################################################
+
+pluginreg_2 <- glmer(Priv_Formalidad_laboral ~ Area + tipo_viv_casa + 
+                       tipo_viv_depto + tipo_viv_cuarto + tipo_viv_indigena + 
+                       mpared1 + mpared2 + mpared3 + mpared4 + mpared5 + 
+                       mpared6 + mpisos1 + mpisos2 + mpisos3 + mpisos4 + 
+                       mpisos5 + agua_alim1 + agua_alim2 + agua_alim3 + 
+                       agua_alim4 + agua_alim5 + agua_alim6 + jefe_Mujer + 
+                       prop_mujeres + electrica_ee + acueducto + 
+                       nhijos_hogar2 + nhijos_hogar3 + nhijos_hogar4 + 
+                       alcantarillado + prop_alfabeta + gasnatural_redp + 
+                       rec_basura + internet + servhig1 + servhig2 + 
+                       servhig3 + servhig4 + servhig5 + tamhog2 + tamhog3 + 
+                       tamhog4 + hacinamiento + prep_alim2 + prep_alim3 + 
+                       prep_alim4 + prep_alim5 + prep_alim6 + Edad_jefe1 + 
+                       Edad_jefe2 + Edad_jefe3 + Edad_jefe4  + inasistente + 
+                       Jefe_sup + ratio_prim + ratio_media + ratio_sup + 
+                       prop_ocupados + prop_inactivos + trabajo_infantil + 
+                       r_solt + r_casad + migrante_medianop + migrante_cortop +
+                       (1|Municipio), family = "binomial", data = Xencuesta)
+
+#------------------------------------------------------------------------------#
+#----------------- Exportando salidas: Modelo Plugin ajustado -----------------#
+#------------------------------------------------------------------------------#
+
+###--- Modelo para aseguramiento en salud ---###
+
+saveRDS(pluginreg_1, file = "PluginSaludMunicipio.rds")
+
+###--- Modelo para formalidad laboral ---###
+
+saveRDS(pluginreg_2, file = "PluginLaboralMunicipio.rds")
+
+#------------------------------------------------------------------------------#
+#-------------------- Cargando los modelos Plugin ajustados -------------------#
+#------------------------------------------------------------------------------#
+
+###--- Modelo para aseguramiento en salud ---###
+
+pluginreg_1 <- readRDS("PluginSaludMunicipio.rds")
+
+###--- Modelo para formalidad laboral ---###
+
+pluginreg_2 <- readRDS("PluginLaboralMunicipio.rds")
+
+###--- Número de municipios en la encuesta ---###
+
+sum(unique(Xencuesta$Municipio) %in% unique(Xcenso$Municipio))
+
+#------------------------------------------------------------------------------#
+#------------ Extracción de los efectos fijos comunes a los dominios ----------#
+#------------------------------------------------------------------------------#
+
+###--- Modelo para aseguramiento en salud ---###
+
+betas1 = as.matrix(fixef(pluginreg_1))
+
+###--- Modelo para formalidad laboral ---###
+
+betas2 = as.matrix(fixef(pluginreg_2))
+
+###------------ Construcción de la matriz censal sintética XBeta ------------###
+
+matrizCenso <- cbind.data.frame(Xcenso$Municipio, cbind(1, as.matrix(Xcenso %>% 
+                                select(rownames(betas1)[-1]))) %*% betas1,
+                                cbind(1, as.matrix(Xcenso %>% select(rownames(betas2)[-1]))) %*% betas2)
+
+colnames(matrizCenso) <- c("Municipio","XB1","XB2")
+head(matrizCenso)
+
+#- Creando el vector nulo para ser reemplazado en el ciclo de estimación Plugin -#
+
+Xcenso$pluginSalud = numeric(14243219)
+Xcenso$pluginFormal = numeric(14243219)
+
+#-- Códigos de los municipios en el censo para el ciclo de estimación Plugin --#
+
+Div = unique(Xcenso$Municipio)
+
+# se definen efectos fijos y efectos aleatorios para cada uno de los dominios 
+
+ud =  data.frame(Municipio = as.numeric(rownames(ranef(pluginreg_1)$Municipio)), 
+                 Salud = ranef(pluginreg_1)$Municipio[[1]],
+                 Formal = ranef(pluginreg_2)$Municipio[[1]])
+
+rownames(ud) <- NULL
+ud$Municipio <- as.factor(ud$Municipio)
+
+ud = data.frame(Municipio = unique(Xcenso$Municipio)) %>% 
+     left_join(ud, by = "Municipio")
+
+ud$Salud[is.na(ud$Salud)] <- 0
+ud$Formal[is.na(ud$Formal)] <- 0
+
+# se estima para cada hogar en la muestra
+
+for(i in 1:length(Div)){
+  print(i)
+  index = which(Xcenso$Municipio == Div[i])
+  
+  Xcenso$pluginSalud[index] = exp(matrizCenso$XB1[index] + ud$Salud[i])/
+                               (1 + exp(matrizCenso$XB1[index] + ud$Salud[i]))
+  Xcenso$pluginFormal[index] = exp(matrizCenso$XB2[index] + ud$Formal[i])/
+                                (1 + exp(matrizCenso$XB2[index] + ud$Formal[i]))
+
+}
+
+#------------------------------------------------------------------------------#
+#--------------- Exportando salidas: Censo con estimación Plugin --------------#
+#------------------------------------------------------------------------------#
+
+saveRDS(Xcenso, file = "Xcenso_plugin.rds")
+
+#------------------------------------------------------------------------------#
+#------------ Cargue de base de datos: Censo con estimación Plugin ------------#
+#------------------------------------------------------------------------------#
+
+Xcenso <- readRDS("Xcenso_plugin.rds")
+
+
+#CensoPersonas %<>% left_join(Xcenso %>% select(idhogar, pluginSalud, pluginFormal), by = "idhogar")
+CensoPersonas %<>% transmute(idhogar, Divipola, Departamento = U_DPTO, 
+                             Area = UA_CLASE) %>% 
+  left_join(Xcenso %>% transmute(idhogar, pluginSalud, pluginFormal), 
+            by = "idhogar")
+
+
+CensoPersonas %>% summarise(PrivSalud = mean(pluginSalud, na.rm = T),
+                            PrivFormal = mean(pluginFormal, na.rm = T))
+
+res_depto <- CensoPersonas %>% group_by(Departamento) %>% summarise(PrivSalud = mean(pluginSalud, na.rm = T),
+                                                       PrivFormal = mean(pluginFormal, na.rm = T))
+
+res_Mun <- CensoPersonas %>% group_by(Divipola) %>% summarise(PrivSalud = mean(pluginSalud, na.rm = T),
+                                                                    PrivFormal = mean(pluginFormal, na.rm = T))
+
+
+write.csv2(res_depto, "Depto.csv")
+write.csv2(res_Mun, "Mun.csv")
