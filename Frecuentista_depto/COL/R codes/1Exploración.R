e7d71db0d98bf96cc1567a7823cc8a69f1424426
Stalyn Guerrero <guerrerostalyn@gmail.com>
2022-07-07 17:24:08 -0400

    Atualizando las validaciones.

--- Frecuentista_depto/COL/R codes/1Exploración.R
+++ Frecuentista_depto/COL/R codes/1Exploración.R
@@ -17,14 +17,16 @@ library(gridExtra)
 library(scales)
 library(formatR)
 library(patchwork)
-
+library(survey)
+library(srvyr)
 theme_set(bayesplot::theme_default())
 
-source(file = "0Funciones/funciones_mrp.R", encoding = "UTF-8")
+source(file = "Frecuentista_depto/0Funciones/funciones_mrp.R",
+       encoding = "UTF-8")
 # Loading data ------------------------------------------------------------
 
-encuesta_ipm <- readRDS("Bayesiana/COL/Data/encuesta_ipm.rds")
-censo_ipm <- readRDS("Bayesiana/COL/Data/censo_ipm.rds")
+encuesta_ipm <- readRDS("Frecuentista_depto/COL/Data/encuesta_ipm.rds")
+censo_ipm <- readRDS("Frecuentista_depto/COL/Data/censo_ipm2.rds")
 
 # Exploratory data analysis -----------------------------------------------
 
@@ -37,14 +39,9 @@ age_plot <-
                by = "edad")
 ### Sex ###
 sex_plot <-
-  Plot_Compare(dat_censo = censo_ipm,
+  Plot_Compare(dat_censo = censo_ipm %>% mutate(sexo = as.character(sexo)),
                dat_encuesta = encuesta_ipm,
                by = "sexo")
-### Level of schooling (LoS) ###
-escolar_plot <-
-  Plot_Compare(dat_censo = censo_ipm,
-               dat_encuesta = encuesta_ipm,
-               by = "anoest")
 
 ### States ###
 depto_plot <-
@@ -53,8 +50,54 @@ depto_plot <-
                by = "depto")
 
 #--- Patchwork in action ---#
-(age_plot | sex_plot | escolar_plot) / ( depto_plot)
+(age_plot | sex_plot ) / ( depto_plot)
 
+
+
+# Validaciones por dimensión.  --------------------------------------------
+
+## Resultado Nacional 
+conteo_censo <- censo_ipm  %>%
+  summarise_at(vars(matches("ipm")),~weighted.mean(.,w = n))
+
+diseno <- encuesta_ipm %>%  as_survey_design(weights = fep)
+
+estimacion_encuesta <- diseno  %>% 
+  summarise_at(vars(matches("ipm")),survey_mean) %>% 
+  dplyr::select(!ends_with("_se"))
+
+temp <- bind_rows(list(cesno = conteo_censo, 
+                       encuesta = estimacion_encuesta),.id = "Fuente")
+temp <- gather(temp,key = "Dimension",value = "Value",-Fuente)
+
+
+ggplot(data = temp, aes(x = Dimension,y = Value, fill = Fuente)) +
+  geom_bar(stat = "identity", position="dodge")
+
+## Resultados depto
+conteo_censo <- censo_ipm %>% group_by(depto)  %>%
+  summarise_at(vars(matches("ipm")),~weighted.mean(.,w = n))
+
+
+estimacion_encuesta <- diseno %>% group_by(depto) %>% 
+  summarise_at(vars(matches("ipm")),list(dir = survey_mean)) %>% 
+  dplyr::select(!ends_with("_se"))
+
+temp <- full_join(conteo_censo, estimacion_encuesta)
+
+par(mfrow = c(2,3))
+plot(temp$ipm_Agua, temp$ipm_Agua_dir )  
+abline(a=0,b=1,col=2)
+plot(temp$ipm_Material, temp$ipm_Material_dir )  
+abline(a=0,b=1,col=2)
+plot(temp$ipm_Saneamiento, temp$ipm_Saneamiento_dir )  
+abline(a=0,b=1,col=2)
+plot(temp$ipm_Energia, temp$ipm_Energia_dir )  
+abline(a=0,b=1,col=2)
+plot(temp$ipm_Internet, temp$ipm_Internet_dir )  
+abline(a=0,b=1,col=2)
+plot(temp$ipm_Hacinamiento, temp$ipm_Hacinamiento_dir )  
+abline(a=0,b=1,col=2)
 
 # Interaction effects  ----------------------------------------------------
 
